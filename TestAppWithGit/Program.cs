219b3003772b5fc2349c800956b65d7ce7826b1c
Vadym <vvindjuk7438@gmail.com>
2022-11-24 10:50:36 +0100

    Add project files.

---
+++ TestAppWithGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAppWithGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
